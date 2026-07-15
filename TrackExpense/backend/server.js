d9a4559f57cf7a33471e5c5e05526a01cdd79098
SHIVAM-DEVELOPER-del <syup3266@gmail.com>
2026-07-15 06:56:36 +0530

    server.js

--- TrackExpense/backend/server.js
+++ TrackExpense/backend/server.js
@@ -1,34 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
 
-import userRouter from './routes/userRoute.js';
-import incomeRouter from './routes/incomeRoute.js';
-import expenseRouter from './routes/expenseRoute.js';
-import dashboardRouter from './routes/dashboardRoute.js';
+import userRouter from "./routes/userRoute.js";
+import incomeRouter from "./routes/incomeRoute.js";
+import expenseRouter from "./routes/expenseRoute.js";
+import dashboardRouter from "./routes/dashboardRoute.js";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
-// MIDDLEWARES
-app.use(cors());
+// CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// DB
+// Database
 connectDB();
 
-// ROUTES
+// Routes
 app.use("/api/user", userRouter);
 app.use("/api/income", incomeRouter);
 app.use("/api/expense", expenseRouter);
 app.use("/api/dashboard", dashboardRouter);
 
-app.get('/', (req, res) => {
-    res.send("API WORKING");
+app.get("/", (req, res) => {
+  res.send("API WORKING");
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
+  console.log(`Server Started on Port ${port}`);
 });
